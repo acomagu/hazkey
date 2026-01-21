8382a3fde6ea4fbc9e56c3e296a36a4a88006d81
Yuki Ito <acomagu@gmail.com>
2026-01-21 17:16:26 +0900

    Add thumb shift token tests

--- fcitx5-hazkey/src/hazkey_config.h
+++ fcitx5-hazkey/src/hazkey_config.h
@@ -5,6 +5,7 @@
 #include <fcitx-config/enum.h>
 #include <fcitx-utils/i18n.h>
 #include <fcitx-utils/library.h>
+#include <fcitx-utils/keysym.h>
 #include <fcitx/menu.h>
 
 namespace fcitx {
@@ -15,6 +16,24 @@ FCITX_CONFIGURATION(HazkeyEngineConfig,
                     Option<bool> showTabToSelect{
                         this, "showTabToSelect",
                         _("Show [Press Tab to Select] indicator"), true};
+                    Option<bool> enableThumbShift{
+                        this, "enableThumbShift", _("Enable thumb shift"),
+                        false};
+                    Option<int> thumbShiftLeftKeySym{
+                        this, "thumbShiftLeftKeySym",
+                        _("Left thumb key keysym"),
+                        static_cast<int>(FcitxKey_Muhenkan)};
+                    Option<int> thumbShiftRightKeySym{
+                        this, "thumbShiftRightKeySym",
+                        _("Right thumb key keysym"),
+                        static_cast<int>(FcitxKey_Henkan)};
+                    Option<int> thumbShiftTimeoutMs{
+                        this, "thumbShiftTimeoutMs",
+                        _("Thumb shift chord timeout (ms)"), 80};
+                    Option<int> thumbShiftTapAction{
+                        this, "thumbShiftTapAction",
+                        _("Thumb tap action (0=none, 1=space, 2=passthrough)"),
+                        1};
                     ExternalOption openHazkeySettings{
                         this, "openHazkeySettings", _("Open Hazkey Settings"),
                         stringutils::concat("hazkey-settings")};);
